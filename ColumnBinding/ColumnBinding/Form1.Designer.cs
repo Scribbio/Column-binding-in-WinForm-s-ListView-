a633e3c0aa5f05e6b06c2ee3e7fc4ae47abaf76f
Scribbio <joeditrolio@outlook.com>
2019-10-15 16:45:33 +0200

    Initial commit

---
+++ ColumnBinding/ColumnBinding/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace ColumnBinding
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lv1 = new System.Windows.Forms.ListView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.btnModify = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lv1
+            // 
+            this.lv1.FullRowSelect = true;
+            this.lv1.HideSelection = false;
+            this.lv1.Location = new System.Drawing.Point(12, 12);
+            this.lv1.Name = "lv1";
+            this.lv1.Size = new System.Drawing.Size(354, 250);
+            this.lv1.TabIndex = 0;
+            this.lv1.UseCompatibleStateImageBehavior = false;
+            this.lv1.View = System.Windows.Forms.View.Details;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 276);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(139, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Location = new System.Drawing.Point(376, 13);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(75, 23);
+            this.btnAddNew.TabIndex = 2;
+            this.btnAddNew.Text = "Add new";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            this.btnAddNew.Click += new System.EventHandler(this.btnAddNew_Click);
+            // 
+            // btnModify
+            // 
+            this.btnModify.Location = new System.Drawing.Point(376, 42);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Size = new System.Drawing.Size(75, 23);
+            this.btnModify.TabIndex = 3;
+            this.btnModify.Text = "Modify";
+            this.btnModify.UseVisualStyleBackColor = true;
+            this.btnModify.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(170, 276);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(139, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(326, 277);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(65, 18);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Enrolled";
+            this.checkBox1.ThreeState = true;
+            this.checkBox1.UseCompatibleTextRendering = true;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(376, 71);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 311);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnModify);
+            this.Controls.Add(this.btnAddNew);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lv1);
+            this.Name = "Form1";
+            this.Text = "Student Enrollment Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lv1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.Button btnModify;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ ColumnBinding/ColumnBinding/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColumnBinding
+{
+    public partial class Form1 : Form
+    {
+
+        // A ContextMenuStrip as member variable.We need to use this instead of the
+        // context menus available in each ListGroup:
+        ContextMenuStrip _ListGroupContextMenu;
+        ToolStripMenuItem _addOption = new ToolStripMenuItem("Add");
+        ToolStripMenuItem _editOption = new ToolStripMenuItem("Edit");
+        ToolStripMenuItem _deleteOption = new ToolStripMenuItem("Delete");
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //  Set up the context menu to use with the GroupedList Items:
+            _ListGroupContextMenu = new ContextMenuStrip();
+
+            // Add some sample ContextMenuStrip Items:
+            _addOption = new ToolStripMenuItem("Add");
+            _addOption.Click += new EventHandler(btnAddNew_Click);
+            _ListGroupContextMenu.Items.Add(_addOption);
+
+            _editOption = new ToolStripMenuItem("Edit");
+            _editOption.Click += new EventHandler(btnEdit_Click);
+            _ListGroupContextMenu.Items.Add(_editOption);
+
+            _deleteOption = new ToolStripMenuItem("Delete");
+            _deleteOption.Click += new EventHandler(btnDelete_Click);
+            _ListGroupContextMenu.Items.Add(_deleteOption);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //// Create a GroupedListControl instance:
+            //ListView lv1 = new ListView();
+            lv1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                | System.Windows.Forms.AnchorStyles.Left)
+                | System.Windows.Forms.AnchorStyles.Right)));
+
+            // Add some sample columns:
+            lv1.Columns.Add(new MyColumnHeader() { Text = "StudentID", Name = "CptEnvE", Width = 80, TextAlign = HorizontalAlignment.Left, Control = textBox1 });
+            lv1.Columns.Add(new MyColumnHeader() { Text = "Faculty", Name = "CptEnvE", Width = 160, TextAlign = HorizontalAlignment.Left, Control = textBox2 });
+            lv1.Columns.Add(new MyColumnHeader() { Text = "Enrolled", Name = "CptEnvE", Width = 60, TextAlign = HorizontalAlignment.Left, Control = checkBox1, SelectValues = "Y|N|N/A" });
+
+            Random rnd = new Random();
+            string[] EnrolledOptions = { "Y", "N", "N/A" };
+
+            // Now add some sample items:
+            for (int j = 1; j <= 5; j++)
+            {
+                ListViewItem item = lv1.Items.Add("Item " + j.ToString());
+                item.SubItems.Add(item.Text + " SubItem 1");
+                item.SubItems.Add(EnrolledOptions[rnd.Next(EnrolledOptions.Length)]);
+            }
+
+            // Add handling for the RightClick Event for contextual menu
+            lv1.MouseClick += new MouseEventHandler(lg_MouseClick);
+            // Add handling for the ListViewItem change Event
+            lv1.ItemSelectionChanged += new ListViewItemSelectionChangedEventHandler(lg_ItemSelectionChanged);
+        }
+
+        public ListViewItem ModifyItem()
+        {
+            ListViewItem item = null;
+            if (lv1.SelectedItems.Count == 1)
+            {
+                item = lv1.SelectedItems[0];
+                foreach (MyColumnHeader column in lv1.Columns)
+                {
+                    string text = column.GetTextFromDispatching();
+                    if (text != null)
+                    {
+                        item.SubItems[column.Index].Text = text;
+                    }
+                }
+            }
+
+            return item;
+        }
+
+
+        public ListViewItem AddItem(int itemIndex = -1)
+        {
+            ListViewItem ClsAddItemRet = default(ListViewItem);
+            var subitems = new string[lv1.Columns.Count - 1 + 1];
+            foreach (MyColumnHeader column in lv1.Columns)
+            {
+                string text = column.GetTextFromDispatching();
+                if (text != null)
+                    subitems[column.Index] = text;
+                else
+                    subitems[column.Index] = "";
+            }
+            var item = new ListViewItem(subitems);
+            if (itemIndex >= 0)
+                ClsAddItemRet = lv1.Items.Insert(itemIndex, item);
+            else
+            {
+                ClsAddItemRet = lv1.Items.Add(item);
+                item.EnsureVisible();
+            }
+            return ClsAddItemRet;
+        }
+
+        private void Dispatching()
+        {
+            if (lv1.SelectedItems.Count == 1)
+            {
+                foreach (ColumnHeader column in lv1.Columns)
+                {
+                    if (column.GetType().Equals(typeof(MyColumnHeader)))
+                    {
+                        var uColumn = (MyColumnHeader)column;
+                        Control controler = uColumn.Control;
+                        if (controler != null)
+                        {
+                            ListViewItem item = lv1.SelectedItems[0];
+                            if (controler.GetType().GetProperty("CheckState") != null)
+                            {
+                                controler.GetType().GetProperty("CheckState").SetValue(controler, uColumn.GetCheckState(item.SubItems[column.Index].Text));
+                            }
+                            else { 
+                                controler.Text = item.SubItems[column.Index].Text;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // Events
+        void lg_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            ListView lv = (ListView)sender;
+
+            ListViewHitTestInfo info = lv.HitTest(e.X, e.Y);
+            ListViewItem item = info.Item;
+
+            if (e.Button == MouseButtons.Right)
+            {
+                // Tuck the Active ListGroup into the Tag property:
+                _ListGroupContextMenu.Tag = lv;
+
+                // Make sure the Delete and Edit options are enabled:
+                _editOption.Enabled = true;
+                _deleteOption.Enabled = true;
+
+                // Because we are not using the GroupedList's own ContextMenuStrip, 
+                // we need to use the PointToClient method so that the menu appears 
+                // in the correct position relative to the control:
+                _ListGroupContextMenu.Show(lv, lv.PointToClient(MousePosition));
+            }
+
+            //Dispatching();
+        }
+
+        void lg_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            Dispatching();
+        }
+
+        private void btnAddNew_Click(object sender, EventArgs e)
+        {
+            AddItem();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            ModifyItem();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            foreach(ListViewItem item in lv1.SelectedItems)
+            { 
+                lv1.Items.Remove(item);
+            }
+
+        }
+    }
+}
